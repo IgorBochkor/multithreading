45b5dac180b5eaf474e2b433fe5aee60888676f7
Igor Bochkor <igorbochkor@gmail.com>
2021-02-22 17:50:07 +0200

    fixed checkstyle mistakes

--- src/main/java/com/dev/Main.java
+++ src/main/java/com/dev/Main.java
@@ -3,7 +3,6 @@ package com.dev;
 public class Main {
     public static void main(String[] args) {
         Counter counter = new Counter();
-        counter.setCounter(0);
         MyThread myThread = new MyThread(counter);
         MyRunnable myRunnable = new MyRunnable(counter);
         myThread.start();
