10e44c90aa220ed5e2fe5d8c812164880adc6997
Igor Bochkor <igorbochkor@gmail.com>
2021-02-24 21:17:11 +0200

    fixed checkstyle mistakes

--- src/test/java/com/dev/task2/MyExecutorServiceTest.java
+++ src/test/java/com/dev/task2/MyExecutorServiceTest.java
@@ -1,21 +1,24 @@
 package com.dev.task2;
 
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MyExecutorServiceTest {
-    private static MyExecutorService myExecutorService;
+    private MyExecutorService myExecutorService;
+    private List<Integer> integerList;
 
     @BeforeEach
     void setUp() {
-        myExecutorService = new MyExecutorService(Util.fillArray());
+        integerList = Util.fillArray();
+        myExecutorService = new MyExecutorService(integerList);
     }
 
     @Test
     void sum_Ok() {
         int actual = myExecutorService.execute();
-        int expected = Util.listSum(Util.fillArray());
+        int expected = Util.listSum(integerList);
         assertEquals(expected, actual);
     }
 }
--- src/test/java/com/dev/task2/MyForkJoinTest.java
+++ src/test/java/com/dev/task2/MyForkJoinTest.java
@@ -1,20 +1,23 @@
 package com.dev.task2;
 
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MyForkJoinTest {
-    private static MyForkJoin myForkJoin;
+    private MyForkJoin myForkJoin;
+    private List<Integer> integerList;
 
     @BeforeEach
     void setUp() {
-        myForkJoin = new MyForkJoin(Util.fillArray());
+        integerList = Util.fillArray();
+        myForkJoin = new MyForkJoin(integerList);
     }
 
     @Test
     void sum_Ok() {
-        int expected = Util.listSum(Util.fillArray());
+        int expected = Util.listSum(integerList);
         int actual = myForkJoin.execute();
         assertEquals(expected, actual);
     }
